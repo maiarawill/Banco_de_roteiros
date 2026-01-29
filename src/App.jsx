fb62d51f0191ba5de93297ae98d88cd16a754f16
Israel da Rosa Barcelos <israelbarcelos6@gmail.com>
2026-01-29 15:28:55 -0300

    adicionado layout de botoes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import { useState } from "react";
 import "./App.css";
 
-function App() {
-  return <></>;
+function App(categorias) {
+  return (
+    <>
+      <div>
+        <div className="title">
+          <h1>Produção de Roteiro</h1>
+        </div>
+        <div className="categorias">
+          {categorias.map((categoria) => (
+            <div className="corpo">
+              <div className="titulo">{categoria.titulo}</div>
+              <div className="botoes">
+                {categoria.botoes.map((botao) => (
+                  <button key={botao.id}>{botao.nome}</button>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
