89993dbb0ef0b98f96b3f1031157f200371e840d
Israel da Rosa Barcelos <israelbarcelos6@gmail.com>
2026-01-29 22:09:12 -0300

    gerenciando estado dos botoes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { use, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Button from "./components/Button";
 
@@ -7,30 +7,38 @@ import buscarCategorias from "./service/categorias";
 const categorias = await buscarCategorias();
 
 function App() {
- 
   //que as categorias sejam inicializadas com um estado
-  const [categoriasRecuperadas, setCategoriasRecuperadas] = useState(categorias);
+  const [categoriasRecuperadas, setCategoriasRecuperadas] =
+    useState(categorias);
 
   return (
-       <div className="bg-indigo-100 w-screen h-screen p-3">
-        <div className="title font-sans font-bold text-4xl m-4 text-indigo-900">
-          <h1>Produção de Roteiro</h1>
-        </div>
-        <div className="categorias">
-          {categorias.map((categoria) => (
-            <div className="corpo" key={categoria.titulo}>
-              <div className="titulo font-sans font-bold text-2xl text-indigo-900">{categoria.titulo}</div>
-              <div className="botoes flex flex-row">
-                {categoria.botoes.map((botao) => {
-                  return(
-                  <Button key={botao.id} nome={botao.nome} selecionado={botao.selecionado} setCategoriasRecuperadas={setCategoriasRecuperadas}  />
-                )})}
-              </div>
+    <div className="bg-indigo-100 w-screen h-screen p-3">
+      <div className="title font-sans font-bold text-4xl m-4 text-indigo-900">
+        <h1>Produção de Roteiro</h1>
+      </div>
+      <div className="categorias">
+        {categoriasRecuperadas.map((categoria) => (
+          <div className="corpo" key={categoria.titulo}>
+            <div className="titulo font-sans font-bold text-2xl text-indigo-900">
+              {categoria.titulo}
             </div>
-          ))}
-        </div>
-      </div> 
-//PROXIMA TAREFA USE STATE/ON CLICK PARA SABER QUAL BOTÃO EU CLIQUEI
+            <div className="botoes flex flex-row">
+              {categoria.botoes.map((botao) => {
+                return (
+                  <Button
+                    key={botao.id}
+                    id={botao.id}
+                    nome={botao.nome}
+                    selecionado={botao.selecionado}
+                    setCategoriasRecuperadas={setCategoriasRecuperadas}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,46 @@
-function Button({nome, selecionado, setCategoriasRecuperadas}) {
+function Button({ id, nome, selecionado, setCategoriasRecuperadas }) {
+  function mudarSelecionado() {
+    if (selecionado === true) {
+      console.log(nome + "Está selecionado");
+    } else {
+      console.log(nome + "Não está selecionado");
+    }
 
-    function mudarSelecionado(){
-        if(selecionado === true){
-    console.log(nome + "Está selecionado") }
-        else{
-    console.log(nome + "Não está selecionado") }   
+    mudarEstadoSelecionado();
   }
-    return(
-        <div>
-            <button className="bg-indigo-800 text-white px-4 py-2 rounded rounded-2xl m-1 hover:bg-indigo-950" onClick={mudarSelecionado} >{nome}</button>
-        </div>
-    )
+
+  const mudarEstadoSelecionado = () => {
+    setCategoriasRecuperadas((categoriasAntigas) => {
+      return categoriasAntigas.map((titulos) => ({
+        ...titulos,
+        botoes: titulos.botoes.map((botao) =>
+          botao.id === id
+            ? { ...botao, selecionado: !botao.selecionado }
+            : botao,
+        ),
+      }));
+    });
+  };
+
+  return (
+    <div>
+      {selecionado ? (
+        <button
+          className="bg-indigo-800 text-white px-4 py-2 rounded rounded-2xl m-1 hover:bg-indigo-950"
+          onClick={mudarSelecionado}
+        >
+          {nome}
+        </button>
+      ) : (
+        <button
+          className="bg-indigo-200 text-white px-4 py-2 rounded rounded-2xl m-1 hover:bg-indigo-950"
+          onClick={mudarSelecionado}
+        >
+          {nome}
+        </button>
+      )}
+    </div>
+  );
 }
 
-export default Button
-
+export default Button;
